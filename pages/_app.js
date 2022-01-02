e6d824d53c9b63d9e7555dbee15e2a28fe78fdb1
dbybus <dima.baibus@gmail.com>
2022-01-03 00:19:03 +0100

    Init commit portofolio

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,13 @@
 import '../styles/index.css'
 import {TodosProvider} from "../contexts/TodosContext"
 import { UserProvider } from "@auth0/nextjs-auth0";
-import Layout from '../components/Layout';
 export default function MyApp({ Component, pageProps }) {
   return (
-    <UserProvider>
       <TodosProvider>
-          <div className="container mx-auto my-10">
-            <Layout>
-                <Component {...pageProps} />
-            </Layout>
-          </div>
+        <div className="container mx-auto my-10 max-w-xl">
+          <Component {...pageProps} />
+        </div>
       </TodosProvider>
-    </UserProvider>
+
   );
 }
